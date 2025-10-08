86982c5617df4934a58a9bb3b35f3253b6355b36
ElioAOT <aotlevai01@gmail.com>
2025-10-08 15:33:43 +0530

    Initial and Huskey Test

---
+++ src/app/__test__/add.test.ts
@@ -0,0 +1,5 @@
+import add from '@/app/add';
+
+test('check add function', () => {
+  expect(add(10, 20)).toBe(30);
+});
---
+++ src/app/add.ts
@@ -0,0 +1,6 @@
+// src/utils/add.js
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+export default add;
